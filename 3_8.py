747e202660a39e742ea8fddfb6c5fa9f110c8a19
Polina Kochergina <kpolina0208@gmail.com>
2022-11-17 20:05:00 +0300

    done!

--- 3_8.py
+++ 3_8.py
@@ -11,12 +11,6 @@ parser.add_argument("h", help = "width and high")
 parser.add_argument("name", help = "name of file") 
 args = parser.parse_args()
 
-# ra = str(args.ra)
-# dec = str(args.dec)
-# w = str(args.w)
-# h = str(args.h)
-# name = str(args.name)
-
 # используем url для доступа к серверу
 site = f"http://skyserver.sdss.org/dr14/SkyServerWS/ImgCutout/getjpeg?TaskName=Skyserver.Explore.Image&ra={args.ra}&dec={args.dec}&scale=0.5&width={args.w}&height={args.h}&opt=G"
 urlretrieve(site, f"{args.name}.jpeg")
