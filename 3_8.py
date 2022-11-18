7f9ab87ad92a200aa7e7a92e1857af0d0b052268
Polina Kochergina <kpolina0208@gmail.com>
2022-11-18 20:58:41 +0300

    del

--- 3_8.py
+++ 3_8.py
@@ -1,5 +1,5 @@
 from urllib.request import urlretrieve
-import argparse
+import argparse 
 
 # описываем аргументы командной строки
 parser = argparse.ArgumentParser() 
@@ -10,7 +10,6 @@ parser.add_argument("w", help = "width and high")
 parser.add_argument("h", help = "width and high") 
 parser.add_argument("name", help = "name of file") 
 args = parser.parse_args()
-
 
 # используем url для доступа к серверу
 site = f"http://skyserver.sdss.org/dr14/SkyServerWS/ImgCutout/getjpeg?TaskName=Skyserver.Explore.Image&ra={args.ra}&dec={args.dec}&scale=0.5&width={args.w}&height={args.h}&opt=G"
